60847b136239b41c6f66dd05d84c306f02e56ee6
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2022-12-21 06:21:10 +0100

    Everything by paged export and docs in place

--- sacexport/sacexport.py
+++ sacexport/sacexport.py
@@ -9,6 +9,33 @@ class RESTError(ValueError):
 class OAuthError(ValueError):
     pass
 
+class RESTParamsError(ValueError):
+    pass
+
+
+class FilterOperators(object):
+    EQUAL = "eq"
+    NOT_EQUAL = "ne"
+    GREATER_THAN = "gt"
+    LESS_THAN = "lt"
+    GREATER_THAN_OR_EQUAL = "ge"
+    LESS_THAN_OR_EQUAL = "le"
+
+
+class StringFilters(object):
+    CONTAINS = "contains"
+    STARTS_WITH = "startswith"
+    ENDS_WITH = "endswith"
+
+class FilterLogicGateSymbols(object):
+    LG_AND = "and"
+    LG_OR = "or"
+    LG_NOT = "not"
+
+
+logicGate = FilterLogicGateSymbols()
+
+
 class SACProvider(object):
     def __init__(self, providerID, providerName, description, serviceURL):
         self.providerID = providerID
@@ -24,6 +51,8 @@ class ModelMetadata(object):
     accounts = {}
     versions = {}
 
+
+
 class SACConnection(object):
     def __init__(self, tenantName, dataCenter):
         self.tenantName = tenantName
@@ -37,6 +66,21 @@ class SACConnection(object):
         self.providers = {}
         self.providerLookup = {}
         self.modelMetadata = {}
+
+        #Filters
+        self.filterDimensionList = {}
+        self.paramManualOverride = {}
+        self.LG_AND = "and"
+        self.LG_OR = "or"
+        self.LG_NOT = "not"
+        self.filterTopN = {}
+        self.filterOrderBy = {}
+        self.filters = {}
+        self.filterLogicGates = {}
+        self.filterOperators = FilterOperators()
+        self.filterStringOperations = StringFilters()
+        self.logicGateOperators = FilterLogicGateSymbols()
+
 
     def getAccessToken(self, clientID, clientSecret):
         self.clientID = clientID
@@ -124,6 +168,97 @@ class SACConnection(object):
         except Exception as e:
             raise e
 
+
+    def addFilterProvider(self, providerID):
+        self.paramManualOverride[providerID] = None
+        self.filterOrderBy[providerID] = {}
+        self.filterLogicGates[providerID] = self.logicGateOperators.LG_AND
+        self.filters[providerID] = []
+
+    def addStringFilter(self, providerID, columnName, filterValue, operator):
+        dateDimensions = list(self.modelMetadata[providerID].dateDimensions.keys())
+        dimensions = list(self.modelMetadata[providerID].dimensions.keys())
+        accounts = list(self.modelMetadata[providerID].accounts.keys())
+        if (operator != self.filterStringOperations.CONTAINS) and (operator != self.filterStringOperations.ENDS_WITH) and (operator != self.filterStringOperations.STARTS_WITH):
+            errMessage = "Invalid value '%s' passed to string filter operator.  Operator must be one of 'contains', 'startswith', or 'endswith'" %operator
+            raise RESTParamsError(errMessage)
+        elif (columnName not in dateDimensions) and (columnName not in dimensions) and (columnName not in accounts) and (columnName not in self.modelMetadata[providerID].measures):
+            validCols = []
+            validCols.extend(dateDimensions)
+            validCols.extend(dimensions)
+            validCols.extend(accounts)
+            validCols.extend(self.modelMetadata[providerID].measures)
+            errMessage = "Invalid value '%s' passed as dimension, mrasure or account column selection.  Valid values for this model are %s" % (columnName, validCols)
+            raise RESTParamsError(errMessage)
+        else:
+            filterSubstring = "%s(%s,'%s')" %(operator, columnName, filterValue)
+            if (len(self.filters[providerID]) < 1) and (self.filterLogicGates[providerID] == self.LG_NOT):
+                filterSubstring = "%s %s" %(self.filterLogicGates[providerID], filterSubstring)
+            elif (len(self.filters[providerID]) > 0) and (self.filterLogicGates[providerID] == self.LG_NOT):
+                filterSubstring = " and (%s %s)" %(self.filterLogicGates[providerID], filterSubstring)
+            elif (len(self.filters[providerID]) > 0):
+                filterSubstring = " %s %s" %(self.filterLogicGates[providerID], filterSubstring)
+            self.filters[providerID].append(filterSubstring)
+
+
+    def setFilterOrderBy(self, providerID, orderByCol, ascDesc):
+        if (ascDesc != "asc") and (ascDesc != "desc"):
+            errMessage = "Invalid value '%s' passed to orderby operator.  Operator must be one of 'asc', or 'desc'" %ascDesc
+            raise RESTParamsError(errMessage)
+        else:
+            self.filterOrderBy[providerID][orderByCol] = ascDesc
+
+    def addLogicalFilter(self, providerID, columnName, filterValue, operator):
+        dateDimensions = list(self.modelMetadata[providerID].dateDimensions.keys())
+        dimensions = list(self.modelMetadata[providerID].dimensions.keys())
+        accounts = list(self.modelMetadata[providerID].accounts.keys())
+        if (operator != self.filterOperators.EQUAL) and (operator != self.filterOperators.NOT_EQUAL) and (operator != self.filterOperators.LESS_THAN) and (operator != self.filterOperators.LESS_THAN_OR_EQUAL) and (operator != self.filterOperators.GREATER_THAN) and (operator != self.filterOperators.GREATER_THAN_OR_EQUAL) and (operator != self.filterOperators.NOT_EQUAL):
+            errMessage = "Invalid value '%s' passed to logical filter operator.  Operator must be one of 'eq', 'ne', 'gt', 'lt', 'ge', or 'le'" %operator
+            raise RESTParamsError(errMessage)
+        elif (columnName not in dateDimensions) and (columnName not in dimensions) and (columnName not in accounts) and (columnName not in self.modelMetadata[providerID].measures):
+            validCols = []
+            validCols.extend(dateDimensions)
+            validCols.extend(dimensions)
+            validCols.extend(accounts)
+            validCols.extend(self.modelMetadata[providerID].measures)
+            errMessage = "Invalid value '%s' passed as dimension, mrasure or account column selection.  Valid values for this model are %s" %(columnName, validCols)
+            raise RESTParamsError(errMessage)
+        else:
+            filterSubstring = "%s %s '%s'" %(columnName, operator, filterValue)
+            if (len(self.filters[providerID]) < 1) and (self.filterLogicGates[providerID] == self.LG_NOT):
+                filterSubstring = "%s %s" %(self.filterLogicGates[providerID], filterSubstring)
+            elif (len(self.filters[providerID]) > 0) and (self.filterLogicGates[providerID] == self.LG_NOT):
+                filterSubstring = " and (%s %s)" %(self.filterLogicGates[providerID], filterSubstring)
+            elif (len(self.filters[providerID]) > 0):
+                filterSubstring = " %s %s" %(self.filterLogicGates[providerID], filterSubstring)
+            self.filters[providerID].append(filterSubstring)
+
+
+    def setParamOverride(self, providerID, moValue):
+        self.paramManualOverride[providerID] = moValue
+
+    def clearParamOverride(self, providerID):
+        self.paramManualOverride[providerID] = None
+
+    def resolveFilter(self, providerID):
+        returnVal = "?"
+        ampersand = ""
+        if self.paramManualOverride[providerID] is not None:
+            returnVal = "%s%s" %(returnVal, self.paramManualOverride[providerID])
+            return returnVal
+        else:
+            if self.filterOrderBy[providerID]:
+                providerOrderByCol = list(self.filterOrderBy[providerID].keys())[0]
+                providerOrderDir = self.filterOrderBy[providerID][providerOrderByCol]
+                returnVal = "%s%s$orderby=%s %s" % (returnVal, ampersand, providerOrderByCol, providerOrderDir)
+                ampersand = "&"
+            if len(self.filters[providerID]) > 0:
+                returnVal = "%s%s$filter=" %(returnVal, ampersand)
+                for currentFilter in self.filters[providerID]:
+                    returnVal = "%s%s" %(returnVal, currentFilter)
+            return returnVal
+
+
     def getModelMetadata(self, providerID):
         try:
             modelMetadata = ModelMetadata()
@@ -185,6 +320,8 @@ class SACConnection(object):
                             if prAtt not in modelMetadata.dimensions:
                                 modelMetadata.measures.append(prAtt)
             self.modelMetadata[providerID] = modelMetadata
+            dimList = list(modelMetadata.dimensions.keys())
+            self.addFilterProvider(providerID)
             return modelMetadata
         except Exception as e:
             errorMsg = "Unknown error during token acquisition."
@@ -215,7 +352,8 @@ class SACConnection(object):
 
     def getFactData(self, providerID):
         try:
-            urlFactData = self.urlProviderRoot + "/" + providerID + "/FactData"
+            filterString = self.resolveFilter(providerID)
+            urlFactData = self.urlProviderRoot + "/" + providerID + "/FactData" + filterString
             response = self.oauth.get(urlFactData)
             responseJson = json.loads(response.text)
             return responseJson["value"]
@@ -227,3 +365,33 @@ class SACConnection(object):
             else:
                 errorMsg = "%s  %s" %(errorMsg, e.error)
                 raise Exception(errorMsg)
+
+if __name__ == "__main__":
+    sac = SACConnection("appdesign", "eu10")
+    sac.connect("sb-90703045-f504-4fed-b5b3-1a408e309e85!b16780|client!b3650", "Qlpl07Z7ehH9EDoTLJ21nZofBys=")
+
+
+    """
+    # New Model
+    hits = sac.searchProviders("TechEd2021")
+    md = sac.getModelMetadata('Cdlvmnd17edjumrnshekknxo8w')
+    sac.addStringFilter('Cdlvmnd17edjumrnshekknxo8w', "Region", "Inter", sac.filterStringOperations.STARTS_WITH)
+    sac.addLogicalFilter('Cdlvmnd17edjumrnshekknxo8w', "NationalParkUnitType", "National Park", sac.filterOperators.EQUAL)
+    sac.addLogicalFilter('Cdlvmnd17edjumrnshekknxo8w', "Date", "197901", sac.filterOperators.EQUAL)
+    sac.setFilterOrderBy('Cdlvmnd17edjumrnshekknxo8w', "State", "asc")
+    sac.setParamOverride('Cdlvmnd17edjumrnshekknxo8w', "$top=5&$orderby=State asc&$filter=startswith(Region,'Inter') and State ne 'CO'")
+    sac.clearParamOverride('Cdlvmnd17edjumrnshekknxo8w')
+    md = sac.getFactData('Cdlvmnd17edjumrnshekknxo8w')
+    """
+
+    """
+    # Account Model
+    hits = sac.searchProviders("BestRunJuice_SampleModel")
+    md = sac.getModelMetadata('sap.epm:BestRunJuice_SampleModel')
+    sac.addLogicalFilter('sap.epm:BestRunJuice_SampleModel', "Account_BestRunJ_sold", "Quantity_sold", sac.filterOperators.EQUAL)
+    sac.addLogicalFilter('sap.epm:BestRunJuice_SampleModel', "Product_3e315003an", "PD1", sac.filterOperators.EQUAL)
+    sac.setFilterOrderBy('sap.epm:BestRunJuice_SampleModel', "Store_3z2g5g06m4", "asc")
+    md = sac.getFactData('sap.epm:BestRunJuice_SampleModel')
+    """
+
+    hello = "world"
